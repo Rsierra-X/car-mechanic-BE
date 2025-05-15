5e35675cc5dbf84dcad66f4c75c6657fea50bbbd
raul sierra <jraulsierra@gmail.com>
2025-05-15 15:20:15 -0600

    commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -23,7 +23,7 @@ import {ServicioModule} from './servicio/servicio.module';
       password: 'root',
       database: 'tallermecanicodb',
       entities: [__dirname + '/**/*.entity.{ts,js}'],
-      synchronize: false, // ¡Cuidado! En producción, usa migraciones syncronize = false
+      synchronize: true, // ¡Cuidado! En producción, usa migraciones syncronize = false
       autoLoadEntities: true,
       logging: ['error', 'warn'],
     }),
